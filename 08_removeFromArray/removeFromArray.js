1ba20e5fa3b866bf153ea03922b9e7850b0df2e7
Inshaal <inshaal81@gmail.com>
2025-07-26 09:07:59 +0500

    completed exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,10 +1,5 @@
-const removeFromArray = function(arr, ...args) {
-    for (let i = arr.length - 1; i >= 0; i--) {
-            if (args.includes(arr[i])) {
-                arr.slice(i, 1);
-            }
-        }
-    return arr;
+const removeFromArray = function(array, ...args) {
+    return array.filter(val => !args.includes(val))
 };
 
 // Do not edit below this line
