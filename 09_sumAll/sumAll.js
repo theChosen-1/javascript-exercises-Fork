e6b9f0dec4c7d1fa168d7196a7d2d1174a5cb2ec
Inshaal <inshaal81@gmail.com>
2025-07-26 10:02:57 +0500

    completed exercises 8-11

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let sum = int1
+    let num = int1
+    while (num != int2) {
+        num += 1
+        sum += num
+    }
+    return sum
 };
-
 // Do not edit below this line
 module.exports = sumAll;
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFaren) {
+  let tempConverted = (tempFaren - 32) * 5/9;
+  return Math.round(tempConverted * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCels) {
+  let tempConverted = (tempCels * 9/5) + 32;
+  return Math.round(tempConverted * 10) / 10;
 };
 
-// Do not edit below this line
+// Do not edit below   this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
